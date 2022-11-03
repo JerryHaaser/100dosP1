565c598a93181a3226a93be1ea1fb7f36615511a
Jerry Haaser <jerry.haaser@hotmail.com>
2022-11-03 11:12:48 -0500

    Set up part 1 of project 1

---
+++ 100dosP1/100dosP1/DetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewController.swift
+//  100dosP1
+//
+//  Created by Jerry haaser on 11/3/22.
+//
+
+import Foundation
---
+++ 100dosP1/100dosP1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  100dosP1
+//
+//  Created by Jerry haaser on 11/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
